34bc52f96ebcea3bffea5cdf40e48c735352e136
zBiggui <bigguisenna@gmail.com>
2021-05-28 20:53:06 -0300

    Lógica expandida para buscar exemplo
    - Buscar exemplo agora é flexível, possui um nível de generalização em que buscará as externas a ela por nível
    > Dessa forma sites que deixam textos separados em blocos podem ser obtidos os textos completos
    - "Mostrar tag pai" alterado para checkbox
    - Mostra tag filho e pai
    - Nova checkbox para tags dividida em partes
    - Adicionado para escolher itens ruins em tags divididas em partes
    - Nova opção extra junto da divisão em partes, para separar em paragrafos (não está 100% ainda)

--- BuscarExemplo/__init__.py
+++ BuscarExemplo/__init__.py
@@ -6,8 +6,119 @@ from ftfy import fix_encoding
 import unicodedata
 
 
-def espaco():
-    st.text('-=' * 45)
+# def espaco():
+#     st.text('-=' * 45)
+
+
+def gerar_textos(t, v):
+    filhos = []
+    filhotes = ['']
+    filhotinhos = ['']
+    testando = []
+    # Limpando as string obtidas, que retorna vazio o none
+
+    # st.text(t)
+
+    englobado = st.checkbox('Selecione se está dividido em partes (provavelmente gerará mais resíduos)', key=v)
+
+    if englobado:
+        separar = st.checkbox('Tentar separar os paragráfos (pode ajudar ou melhorar)', key=v)
+
+    if englobado:
+
+        testers = ''
+        checar = False
+        for tst in t.strings:
+            if tst.strip():
+                testando.append(tst.strip())
+                checar = False
+                if separar:
+                    filhotes[len(filhotes)-1] += f'{tst.strip()} '
+                    # filhos.append(f'{tst.strip()} ')
+                else:
+                    filhotes.append(tst.strip())
+            else:
+                if not checar:
+                    filhotes.append('\n\n')
+                    # filhos.append('\n\n')
+                    checar = True
+
+        container = st.beta_container()
+        all = st.checkbox("Select all", key=v)
+
+
+        if not separar:
+            if all:
+                selected_options = container.multiselect("Selecione uma ou mais opções:",
+                                                         filhotes, filhotes, key=v)
+            else:
+                selected_options = container.multiselect("Selecione uma ou mais opções:",
+                                                         filhotes, key=v)
+        else:
+            if all:
+                selected_options = container.multiselect("Selecione uma ou mais opções:",
+                                                         testando, testando, key=v)
+            else:
+                selected_options = container.multiselect("Selecione uma ou mais opções:",
+                                                         testando, key=v)
+
+        tester = ''
+        if st.button('Printar', key=v):
+            for t in selected_options:
+                # HTML pra usar depois talvez como PDF
+                # print(t)
+                if not separar:
+                    filhotinhos[len(filhotinhos)-1] += f'{t}'
+                else:
+                    tester += f'{t.strip()}\n\n'
+
+            st.markdown('---')
+            if not separar:
+                for x in filhotinhos:
+                    st.write(x)
+            else:
+                st.write(tester)
+            st.markdown('---')
+            # print(tester)
+
+        try:
+            st.text(f'> Pai:\nTag: {t.name}\nAtributos:{t.attrs}')
+        except:
+            pass
+
+        st.markdown('---')
+    else:
+        try:
+            for tste in t:
+                if str(tste.string).strip() and tste.string is not None:
+                    filhos.append(tste)
+                    # print(tste)
+
+            container = st.beta_container()
+            all = st.checkbox("Select all", key=v)
+
+            if all:
+                selected_options = container.multiselect("Selecione uma ou mais opções:",
+                                                         filhos, filhos, key=v)
+            else:
+                selected_options = container.multiselect("Selecione uma ou mais opções:",
+                                                         filhos, key=v)
+
+            tester = ''
+            if st.button('Printar', key=v):
+                for t in selected_options:
+                    # HTML pra usar depois talvez como PDF
+                    # print(t)
+                    tester += f'{t.string.strip()}\n\n'
+
+                st.markdown('---')
+                st.write(f'{tester}')
+                st.markdown('---')
+                # print(tester)
+
+            st.text(f'> Pai:\nTag: {t.name}\nAtributos:{t.attrs}')
+        except AttributeError:
+            st.error("Tente aumentar o nível de generalização")
 
 
 def buscar_exemplo(headers, session_state):
@@ -17,9 +128,11 @@ def buscar_exemplo(headers, session_state):
 
     teste = st.text_input('Texto a ser buscado')
 
-    filhos = []
+    select_slider = st.slider('Nível de generalização', 0, 10)
 
     if st.button('Buscar') or session_state.last_URL == link:
+
+        # st.text(select_slider)
 
         # Checando se o último request do usuário foi feito usando o mesmo Link
         if session_state.last_URL != link:
@@ -35,67 +148,33 @@ def buscar_exemplo(headers, session_state):
             if len(soup.findAll(text=re.compile(teste, re.IGNORECASE))) != 0:
                 for v, t in enumerate(soup.findAll(text=re.compile(teste, re.UNICODE))):
 
-                    espaco()
+                    st.markdown('---')
 
                     # st.text(t)
 
-                    st.text(t.parent)
-
+                    # st.text(t.parent)
+                    #
                     # TAG
-                    st.text(t.parent.name)
+                    # st.text(t.parent.name)
 
-                    mostrar_pai = st.radio('Mostrar tag pai?', ['nao', 'sim'], key=v)
+                    st.text(f'Filho: {t}')
 
-                    if mostrar_pai == 'sim':
+                    mostrar_pai = st.checkbox('Mostrar tag pai?', key=v)
+
+                    if mostrar_pai:
                         # st.text(t.parent.parent)
 
-                        # Limpando as string obtidas, que retorna vazio o none
-                        for tste in t.parent.parent:
-                            if str(tste.string).strip() and tste.string is not None:
-                                filhos.append(tste)
-                                # print(tste)
+                        x = t
+                        for c in range(select_slider):
+                            x = x.parent
 
-                        container = st.beta_container()
-                        all = st.checkbox("Select all", key=v)
-
-                        if all:
-                            selected_options = container.multiselect("Selecione uma ou mais opções:",
-                                                                     filhos, filhos, key=v)
-                        else:
-                            selected_options = container.multiselect("Selecione uma ou mais opções:",
-                                                                     filhos, key=v)
-
-                        tester = ''
-                        if st.button('Printar', key=v):
-                            for t in selected_options:
-                                # HTML pra usar depois talvez como PDF
-                                # print(t)
-                                tester += f'{t.string.strip()}\n\n'
-
-                            st.write(f'{tester}')
-                            print(tester)
-
-
-
-
-
-                        # st.text(t.parent.parent.text)
-                        # for t in t.parent.parent.findAll('p'):
-                        #     print(t)
-                        #     st.text(f'{t.text}\n')
-                        #     st.text(f'{t.text}\n')
-
-                    st.text(f'> Pai:\nTag: {t.parent.parent.name}\nAtributos:{t.parent.parent.attrs}')
-
-                    tag = t.parent.name
-
-                    # st.text(t.parent.attrs)
-
-                    i = t.parent.attrs
-
-                    # st.text(t.next_sibling)
+                        gerar_textos(x, v)
 
                     try:
+
+                        tag = x.name
+
+                        i = x.attrs
                         # st.text(i['class'])
 
                         classe = ''
@@ -113,7 +192,7 @@ def buscar_exemplo(headers, session_state):
                     except:
                         pass
 
-                espaco()
+                st.markdown('---')
             else:
                 st.error('Nada encontrado :(')
     pass
