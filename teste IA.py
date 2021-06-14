e8fb30b809ebced1b14b10a8a6bc343f40687e47
Guilherme Senna Cruz <53920452+GuilhermeSenna@users.noreply.github.com>
2021-06-14 12:24:29 -0300

    Modularização e retira ocorrências manualmente
    - Uso de funções para proporcionar modularização ao código
    - Pega todas os índices das ocorrências na lista que não correspondem a desejada.
    - Retira do dicionário, permitindo uma forma de enxergar as regras

--- teste IA.py
+++ teste IA.py
@@ -1,8 +1,54 @@
 import csv
 from collections import defaultdict
 
-def probabilidade(valor, total):
+
+def calcular_probabilidade(valor, total):
     return f'{(valor/total)*100:.2f}'
+
+
+def mostrar_probabilidade(nomes, printar):
+    for n in nomes:
+        lista = []
+        dicionario = {}
+        total = 0
+
+        for c in range(2):
+            for nome in nomes[n]:
+                total += 1
+                if not lista:
+                    lista.append(nome)
+                if not nome in lista:
+                    lista.append(nome)
+
+                if c == 1:
+                    for k in dicionario.keys():
+                        if k == nome:
+                            dicionario[k] += 1
+
+            if c == 0:
+                dicionario = dict.fromkeys(lista, 0)
+                dicionario['total'] = total
+
+        if printar:
+            print('-=' * 50)
+            print(n)
+            for key, value in dicionario.items():
+                print(f'{key} -> {value}x, {calcular_probabilidade(value, dicionario["total"])}%')
+
+
+def retirar_ocorrencias(nomes, variavel, valor):
+    # Essa lógica permite remover todas as ocorrências que não forem a escolhida
+    # Para poder criar uma lógica
+    indices = [i for i, x in enumerate(nomes[variavel]) if x != valor]
+
+    for key, value in nomes.items():
+        cont = 0
+        for i in indices:
+            nomes[key].pop(i - cont)
+            cont += 1
+
+    return nomes
+
 
 filename = "student-mat.csv"
 
@@ -30,30 +76,9 @@ with open(filename, 'r') as data:
             for x in dates_dict:
                 nomes[f'{keys_list[x]}'] = dates_dict[x]
 
-for n in nomes:
-    lista = []
-    dicionario = {}
-    total = 0
 
-    for c in range(2):
-        for nome in nomes[n]:
-            total += 1
-            if not lista:
-                lista.append(nome)
-            if not nome in lista:
-                lista.append(nome)
+# 1º Passa o dicionário
+# 2º Perguntar se quer printar ou não
+mostrar_probabilidade(nomes, False)
 
-            if c == 1:
-                for k in dicionario.keys():
-                    if k == nome:
-                        dicionario[k] += 1
-
-        if c == 0:
-            dicionario = dict.fromkeys(lista, 0)
-            dicionario['total'] = total
-
-    print(n)
-    for key, value in dicionario.items():
-        print(f'{key} -> {value}x, {probabilidade(value, dicionario["total"])}%')
-
-    print('-='*30)
+mostrar_probabilidade(nomes, True)
